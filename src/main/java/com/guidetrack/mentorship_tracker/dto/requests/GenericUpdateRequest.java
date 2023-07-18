1d770d27bc052696ec6ef33c01f29f76fe321fbb
Methuselah-Nwodobeh <methuselah.nwodobeh@amalitech.com>
2023-07-18 16:58:30 +0000

    fix/move email values to system environment, delete GenericUpdateRequest.java

--- src/main/java/com/guidetrack/mentorship_tracker/dto/requests/GenericUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.guidetrack.mentorship_tracker.dto.requests;
-
-import jakarta.validation.constraints.NotNull;
-
-public record GenericUpdateRequest<T, G>(
-        @NotNull T identifier,
-        @NotNull G replacement
-) {
-}
